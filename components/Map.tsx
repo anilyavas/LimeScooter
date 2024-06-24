a015702fe5cb4d953ef2a91174fd12c5ea5da3fc
anilyavas <anil.yavas99@gmail.com>
2024-06-24 18:39:33 +0300

    expo location

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,14 @@
-import Mapbox, { MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 
 const accessToken =
   'pk.eyJ1IjoiYW5pbHlhdmFzIiwiYSI6ImNseHFocTlndjA4MGgya3NlNDBzYnVwd24ifQ.0stTd8rND6fVJCDjBF1veQ';
 Mapbox.setAccessToken(accessToken);
 
 export default function Map() {
-  return <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11" />;
+  return (
+    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
+      <Camera followUserLocation />
+      <LocationPuck />
+    </MapView>
+  );
 }
