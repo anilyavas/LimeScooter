94888655dd3192622e830657a055a4cb8c8d52a3
anilyavas <anil.yavas99@gmail.com>
2024-06-25 10:25:27 +0300

    install and setup RN Mapbox

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,12 @@
 import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 
-const accessToken =
-  'pk.eyJ1IjoiYW5pbHlhdmFzIiwiYSI6ImNseHFocTlndjA4MGgya3NlNDBzYnVwd24ifQ.0stTd8rND6fVJCDjBF1veQ';
-Mapbox.setAccessToken(accessToken);
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map() {
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
       <Camera followZoomLevel={16} followUserLocation />
-      <LocationPuck />
+      <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
     </MapView>
   );
 }
