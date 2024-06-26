a48ddd25d99a7818a640959dd58f1f8eb5bac310
anilyavas <anil.yavas99@gmail.com>
2024-06-26 15:34:19 +0300

    scooter count display when zoom out

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,6 @@
 import Mapbox, {
   Camera,
+  CircleLayer,
   Images,
   LocationPuck,
   MapView,
@@ -20,13 +21,40 @@ export default function Map() {
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
       <Camera followZoomLevel={10} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
-      <ShapeSource id="scooters" shape={featureCollection(points)}>
+      <ShapeSource
+        id="scooters"
+        cluster
+        shape={featureCollection(points)}
+        onPress={(e) => console.log(JSON.stringify(e, null, 2))}>
+        <SymbolLayer
+          id="clusters-count"
+          style={{
+            textField: ['get', 'point_count'],
+            textSize: 16,
+            textColor: '#ffffff',
+            textPitchAlignment: 'map',
+          }}
+        />
+        <CircleLayer
+          id="clusters"
+          filter={['has', 'point_count']}
+          style={{
+            circleColor: '#42E100',
+            circlePitchAlignment: 'map',
+            circleRadius: 20,
+            circleOpacity: 1,
+            circleStrokeWidth: 2,
+            circleStrokeColor: 'white',
+          }}
+        />
         <SymbolLayer
           id="scooter-icons"
+          filter={['!', ['has', 'point_count']]}
           style={{
             iconImage: 'pin',
             iconSize: 0.5,
             iconAllowOverlap: true,
+            iconAnchor: 'bottom',
           }}
         />
         <Images images={{ pin }} />
